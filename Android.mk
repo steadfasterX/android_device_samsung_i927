1546ea13e4f13297b6eeca972f95b4ebbe4ed05b
Kick-Buttowski <vmarceloe@gmail.com>
2013-07-25 12:38:11 -0300

    proper conditional and doesn't break the i9103

--- Android.mk
+++ Android.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),n1)
+ifeq ($(TARGET_DEVICE),i927)
 
 LOCAL_PATH := $(call my-dir)
 
