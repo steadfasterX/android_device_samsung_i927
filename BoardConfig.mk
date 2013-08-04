976c75a6cc91daee9a39cf2a8e0ad3e5cda711fc
Kick Buttowski <vmarceloe@gmail.com>
2013-08-04 20:35:22 +0200

    charger fixed

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -139,10 +139,12 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/fsl-tegra-udc/gadget/l
 BOARD_HAS_SDCARD_INTERNAL := true
 BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
 
-# LPM
-BOARD_CHARGING_MODE_BOOTING_LPM := "/sys/class/power_supply/battery/batt_lp_charging"
+# LPM charge mode, should be /sys/module/kernel/parameters/lpm_boot but doesnt works
+# /sys/class/power_supply/battery/batt_charging_source equals 2 when a branded charger is connected
+#
+BOARD_CHARGING_MODE_BOOTING_LPM := "/sys/class/power_supply/ac/online"
 BOARD_BATTERY_DEVICE_NAME := "battery"
-BOARD_CHARGER_RES := device/samsung/i927/res/charger
+BOARD_CHARGER_RES := $(LOCAL_PATH)/res/charger
 
 # EMMC brickbug is removed in the kernel, but be better safe than sorry.
 BOARD_SUPPRESS_EMMC_WIPE := true
