933f291164233567b939200c8aa166b1cfb4ad18
bubor <bubor@dualhoki.vim.hu>
2013-11-28 13:18:42 +0100

    slimrom bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_i927.mk
+    $(LOCAL_DIR)/slim_i927.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,4 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo full_i927-eng
-add_lunch_combo omni_i927-eng
+add_lunch_combo slim_i927-userdebug
