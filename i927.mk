8b7aff66d5263d0dfed4ad52edae162c8c145371
bubor <bubor@dualhoki.vim.hu>
2014-01-20 09:22:12 +0100

    graphics: more settings

--- i927.mk
+++ i927.mk
@@ -153,7 +153,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # This is a 16.16 fixed point number
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.zygote.disable_gl_preload=1 \
-    ro.opengles.version=131072
+    ro.opengles.version=196608
 
 # These are the hardware-specific settings that are stored in system properties.
 # Note that the only such settings should be the ones that are too low-level to
