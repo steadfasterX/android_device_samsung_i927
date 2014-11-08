13d7111063ead7c98d06c3a4ed7068ada8bede74
bubor <bubor@dualhoki.vim.hu>
2014-11-08 18:06:29 +0100

    remove framework-base patch fail to apply the patch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -25,16 +25,6 @@
 add_lunch_combo cm_i927-userdebug
 add_lunch_combo pac_i927-userbebug
 
-echo "Apply patch to frameworks/base"
-echo -n "Apply patch 0001-framework-base-patch.patch"
-(cd frameworks/base; git am ../../device/samsung/i927/patches/0001-framework-base-patch.patch) > /dev/null 2>&1
-if [ $? == 0 ]; then
-	echo "     [DONE]"
-else
-	(cd frameworks/base; git am --abort)
-	echo "     [FAIL]"
-fi
-
 echo -n "Apply patch 0002-Add-missing-functions-and-signatures-for-older-OMX-v.patch"
 (cd frameworks/av; git am ../../device/samsung/i927/patches/0002-Add-missing-functions-and-signatures-for-older-OMX-v.patch) > /dev/null 2>&1
 if [ $? == 0 ]; then
